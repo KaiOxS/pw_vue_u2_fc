cc8c3f7a9aaf1307916a304913045732d1ebb459
KaiOxS <torito9926@hotmail.es>
2025-11-26 19:41:18 -0500

    Taller 14: Directivas en Vue

--- src/views/ContadorView.vue
+++ src/views/ContadorView.vue
@@ -1,20 +1,20 @@
 <template>
   <div>
-    <Contador/>
-    <Contador/>
+    <!--V-bind todo lo que está dentro lo transforma a js, su shortcut es (:) los 2 puntos únicamente,
+    se usa también para boleanos no solo para cadenas-->
+    <Contador tit="Contador 1" v-bind:inicio="20" :mostrar="true" />
+    <Contador tit="Contador 2" :inicio="4" :mostrar="false" />
   </div>
 </template>
 
 <script>
-import Contador from '../components/Contador.vue'
+import Contador from "../components/Contador.vue";
 export default {
-    components:{
-    Contador
-    },
-
-}
+  components: {
+    Contador,
+  },
+};
 </script>
 
 <style>
-
 </style>
